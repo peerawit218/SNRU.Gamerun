f1c5f6d7cfca59127a31198e2a1dd878b76a8cb7
peerawit <peerawit@cbwschool.net>
2016-05-15 23:33:28 -0700

    ฉันได้ intent to SignUp แล้ว

--- app/src/main/java/snru/joonsri/peerawit/gamerun/MainActivity.java
+++ app/src/main/java/snru/joonsri/peerawit/gamerun/MainActivity.java
@@ -1,7 +1,9 @@
 package snru.joonsri.peerawit.gamerun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }//Main Method
+
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
     }
-}
+
+
+}//Main Class นี่คือคลาสหลัก
