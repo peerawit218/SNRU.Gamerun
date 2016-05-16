5d770edd2162e957e9dc42b673c33270abc8a0e7
peerawit <peerawit@cbwschool.net>
2016-05-16 00:53:10 -0700

    Intent to SignUp แล้ว

--- app/src/main/java/snru/joonsri/peerawit/gamerun/SignUp.java
+++ app/src/main/java/snru/joonsri/peerawit/gamerun/SignUp.java
@@ -3,10 +3,11 @@ package snru.joonsri.peerawit.gamerun;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
 
-
+    public EditText nameEditText, userEditText, passswordEditText;
 
 
 
