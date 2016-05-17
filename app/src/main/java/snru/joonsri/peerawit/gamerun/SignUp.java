2d3d2ec1aab55b22e66ca3a255fa7e12e10db300
peerawit <peerawit@cbwschool.net>
2016-05-16 23:43:48 -0700

    signup ได้แล้ว

--- app/src/main/java/snru/joonsri/peerawit/gamerun/SignUp.java
+++ app/src/main/java/snru/joonsri/peerawit/gamerun/SignUp.java
@@ -1,19 +1,29 @@
 package snru.joonsri.peerawit.gamerun;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUp extends AppCompatActivity {
 
-
     //Explicit
-    public EditText nameEditText, userEditText, passwordEditText;
+    private EditText nameEditText, userEditText, passwordEditText;
     private RadioGroup radioGroup;
-    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton, choice5RadioButton;
     private String nameString, userString, passwordString, avataString = "0";
 
 
@@ -22,22 +32,21 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-
         //Bind Widget
         bindWidget();
 
         //Radio Button Controller
-
         radioButtonController();
 
 
-    }//Main Method
+
+    }   // Main Method
 
     private void radioButtonController() {
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int i) {
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
 
                 switch (i) {
                     case R.id.radioButton:
@@ -59,7 +68,6 @@ public class SignUp extends AppCompatActivity {
 
             }
         });
-
     }
 
     private void bindWidget() {
@@ -72,7 +80,6 @@ public class SignUp extends AppCompatActivity {
         choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
         choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
         choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
-        choice5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
 
     }
 
@@ -84,17 +91,48 @@ public class SignUp extends AppCompatActivity {
         passwordString = passwordEditText.getText().toString().trim();
 
         //Check Space
-        if (nameString.equals("") ||userString.equals("") || passwordString.equals("")) {
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
 
             MyAlert myAlert = new MyAlert();
-            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง");
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
 
         } else {
 
-
+            updateUserTABLE();
 
         }
 
+
+
     }
 
-}//Main Class
+    private void updateUserTABLE() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .add("Avata", avataString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_master.php").post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+
+
+    }   // updateUserTABLE
+
+}   // Main Class
