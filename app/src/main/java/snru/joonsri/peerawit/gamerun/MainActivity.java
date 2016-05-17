60a2b8d36f44670c326efcb72ced3a3dd407d79b
peerawit <peerawit@cbwschool.net>
2016-05-17 03:04:01 -0700

    ดึง data จากฐานข้อมูลมากทั้งหมด

--- app/src/main/java/snru/joonsri/peerawit/gamerun/MainActivity.java
+++ app/src/main/java/snru/joonsri/peerawit/gamerun/MainActivity.java
@@ -2,9 +2,18 @@ package snru.joonsri.peerawit.gamerun;
 
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +34,73 @@ public class MainActivity extends AppCompatActivity {
         //Delete All SQLite
         deleteAllSQLite();
 
+        //Synchronize
+        MySynchronize mySynchronize = new MySynchronize();
+        mySynchronize.execute();
+
 
     }//Main Method
+
+    //Create Inner Class
+    public class MySynchronize extends AsyncTask<Void, Void, String> {
+
+        //ประกาศ Class ซ้อน Class
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/snru/get_user.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+
+            } catch (Exception e) {
+
+                return null;
+
+            }
+
+            //return null;
+        } //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("Snru", "JSON == " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i = 0; i < jsonArray.length(); i++) {
+
+                    //ดึงข้อมูลมาโชว์จากฐานข้อมูลทั้งหมด
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String strName = jsonObject.getString(myManage.column_name);
+                    String strUser = jsonObject.getString(myManage.column_user);
+                    String strPassword = jsonObject.getString(myManage.column_password);
+                    String strAvata = jsonObject.getString(myManage.column_avata);
+
+                    myManage.addUser(strName, strUser, strPassword, strAvata);
+
+                } //For
+
+            } catch (Exception e) {
+
+                e.printStackTrace();
+
+            }
+
+        } //onPost
+
+
+
+    } //MySyn Class
+
+
 
     //Delete All SQLite
     private void deleteAllSQLite() {
