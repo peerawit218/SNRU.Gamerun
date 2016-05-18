bfa3601f66f64544c81edd4b4a0bdbf43f797826
peerawit <peerawit@cbwschool.net>
2016-05-18 00:51:46 -0700

    ดึง maps มาโชว์ที่สกลนคร

--- app/src/main/java/snru/joonsri/peerawit/gamerun/MapsActivity.java
+++ app/src/main/java/snru/joonsri/peerawit/gamerun/MapsActivity.java
@@ -1,5 +1,6 @@
 package snru.joonsri.peerawit.gamerun;
 
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -9,38 +10,35 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.nearby.sharing.LocalContent;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private double snruLatDouble = 17.190512,
+            snruLanADouble = 104.090424;
+    private LocationManager locationManage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.my_design);//เปิดไปหน้าที่ต้องการ
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    }
+    } //Main Method
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Setup for สกล
+        LatLng snruLatlng = new LatLng(snruLatDouble, snruLanADouble);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(snruLatlng, 16));
+
     }
-}
+} //Main Class
+
