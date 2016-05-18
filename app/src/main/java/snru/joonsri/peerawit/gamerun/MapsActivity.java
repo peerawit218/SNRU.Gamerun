ea7a6788475188bab8845e24716586f273a4b288
peerawit <peerawit@cbwschool.net>
2016-05-18 01:20:52 -0700

    ค้นหาพิกัด เมื่อมีค่าเคลื่อนที่

--- app/src/main/java/snru/joonsri/peerawit/gamerun/MapsActivity.java
+++ app/src/main/java/snru/joonsri/peerawit/gamerun/MapsActivity.java
@@ -1,9 +1,14 @@
 package snru.joonsri.peerawit.gamerun;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -18,6 +23,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private double snruLatDouble = 17.190512,
             snruLanADouble = 104.090424;
     private LocationManager locationManage;
+    private Criteria criteria;
+    private double myLatADouble, myLngADouble;
+    private boolean gpsABoolean, networkABoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +35,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Setup Location
+        locationManage = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+
+
     } //Main Method
+
+    public Location myFindLocation(String strProvider, String strError) {
+
+        Location location = null;
+        if (locationManage.isProviderEnabled(strProvider)) {
+
+            locationManage.requestLocationUpdates(strProvider, 1000, 10, (android.location.LocationListener) locationListener);
+            location = locationManage.getLastKnownLocation(strProvider);
+
+
+
+        } else {
+
+            Log.d("test", "my Error == " + strError);
+
+        }
+        return null;
+    }
+
+
+
+    //Create Inner Class //คลาสซ้อนคลาส
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            //การค้นหา เมื่อมีการเปลี่ยนค่า
+            myLatADouble = location.getLatitude();
+            myLatADouble = location.getLongitude();
+
+        }
+    }; //Location
 
 
 
